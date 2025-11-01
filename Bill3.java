f8110fcac92cf81d4055075cfe8f64bf3a20b256
הילה  זילבר <hila@h-MacBook-Pro-sl-hylh.local>
2025-11-01 23:26:35 +0200

    compile with s

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,6 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
-		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
@@ -10,6 +8,6 @@ public class Bill3 {
 		amount = amount/3;
 		amount = Math.ceil(amount);
 		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay "
-		 + amount + " Shekel each");
+		 + amount + " Shekels each");
 	}
 }
