2e498ff11bb583ae21bd2b088bc9519e0688355e
הילה  זילבר <hila@h-MacBook-Pro-sl-hylh.local>
2025-11-02 11:07:12 +0200

    changed the hundreds calculation

--- NumWords.java
+++ NumWords.java
@@ -6,7 +6,7 @@ public class NumWords {
 		num = num/10;
 		int tens = num%10;
 		num = num/10;
-		int hundreds = num%10;
+		int hundreds = num;
 
 		System.out.printf("%d hundreds, %d tens, and %d ones.",
 		hundreds, tens, units);
